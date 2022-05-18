86666081d1859729b30bfe8239daaebf06445ef6
Ipek Ozbek <ipekozbek@windowslive.comclearq>
2022-05-19 00:19:46 +0300

    Scrool View ayarlandı.

--- lib/consumer.dart
+++ lib/consumer.dart
@@ -34,192 +34,197 @@ class consumer extends StatelessWidget {
                 ),
               ),
             ),
-            body: Container(
-              color: Color.fromRGBO(242, 239, 234, 1),
-              child: Wrap(
-                direction: Axis.horizontal,
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(top: 60, bottom: 70),
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/koyum.png'))),
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Container(
-                      margin: EdgeInsets.only(
-                          left: 20, right: 5, top: 5, bottom: 30),
-                      width: 300,
-                      height: 180,
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(
-                              bottom: 0,
-                              top: 125,
-                            ),
-                            padding:
-                                EdgeInsets.only(left: 10, bottom: 10, top: 15),
-                            width: 300,
-                            height: 55,
-                            child: Text(
-                              'Tüketici Olarak Katıl',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontFamily: 'Quicksand'),
-                            ),
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 5,
-                                  blurRadius: 7,
-                                  offset: Offset(0, 3),
-                                )
-                              ],
-                              borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(20),
-                                  bottomRight: Radius.circular(20)),
-                              color: Color.fromARGB(255, 221, 73, 14),
-                            ),
-                          ),
-                        ],
-                      ),
+            body: SingleChildScrollView(
+              child: Container(
+                color: Color.fromRGBO(242, 239, 234, 1),
+                child: Wrap(
+                  direction: Axis.horizontal,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 60, bottom: 70),
                       decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
-                            )
-                          ],
                           image: DecorationImage(
-                              image: AssetImage('assets/images/tuketici.jpg'),
-                              fit: BoxFit.cover),
-                          color: Color.fromARGB(255, 221, 73, 14),
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                              image: AssetImage('assets/images/koyum.png'))),
                     ),
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Container(
-                      margin: EdgeInsets.only(
-                          left: 20, right: 5, top: 20, bottom: 20),
-                      width: 300,
-                      height: 180,
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(
-                              bottom: 0,
-                              top: 125,
+                    FlatButton(
+                      onPressed: () {},
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            left: 20, right: 5, top: 5, bottom: 30),
+                        width: 300,
+                        height: 180,
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(
+                                bottom: 0,
+                                top: 125,
+                              ),
+                              padding: EdgeInsets.only(
+                                  left: 10, bottom: 10, top: 15),
+                              width: 300,
+                              height: 55,
+                              child: Text(
+                                'Tüketici Olarak Katıl',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    fontFamily: 'Quicksand'),
+                              ),
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3),
+                                  )
+                                ],
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20)),
+                                color: Color.fromARGB(255, 221, 73, 14),
+                              ),
                             ),
-                            padding:
-                                EdgeInsets.only(left: 10, bottom: 10, top: 15),
-                            width: 300,
-                            height: 55,
-                            child: Text(
-                              'Çiftçi Olarak Katıl',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontFamily: 'Quicksand'),
-                            ),
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 5,
-                                  blurRadius: 7,
-                                  offset: Offset(0, 3),
-                                )
-                              ],
-                              borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(20),
-                                  bottomRight: Radius.circular(20)),
-                              color: Color.fromARGB(255, 221, 73, 14),
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(0, 3),
+                              )
+                            ],
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/tuketici.jpg'),
+                                fit: BoxFit.cover),
+                            color: Color.fromARGB(255, 221, 73, 14),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
                       ),
-                      decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
-                            )
-                          ],
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/toprak2.jpg'),
-                              fit: BoxFit.cover),
-                          color: Color.fromARGB(255, 221, 73, 14),
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
                     ),
-                  ),
-                  FlatButton(
-                    onPressed: () {},
-                    child: Container(
-                      margin: EdgeInsets.only(
-                          left: 20, right: 5, top: 5, bottom: 30),
-                      width: 300,
-                      height: 180,
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(
-                              bottom: 0,
-                              top: 125,
+                    FlatButton(
+                      onPressed: () {},
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            left: 20, right: 5, top: 20, bottom: 20),
+                        width: 300,
+                        height: 180,
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(
+                                bottom: 0,
+                                top: 125,
+                              ),
+                              padding: EdgeInsets.only(
+                                  left: 10, bottom: 10, top: 15),
+                              width: 300,
+                              height: 55,
+                              child: Text(
+                                'Çiftçi Olarak Katıl',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    fontFamily: 'Quicksand'),
+                              ),
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3),
+                                  )
+                                ],
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20)),
+                                color: Color.fromARGB(255, 221, 73, 14),
+                              ),
                             ),
-                            padding:
-                                EdgeInsets.only(left: 10, bottom: 10, top: 15),
-                            width: 300,
-                            height: 55,
-                            child: Text(
-                              'Tüketici Olarak Katıl',
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontFamily: 'Quicksand'),
-                            ),
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 5,
-                                  blurRadius: 7,
-                                  offset: Offset(0, 3),
-                                )
-                              ],
-                              borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(20),
-                                  bottomRight: Radius.circular(20)),
-                              color: Color.fromARGB(255, 221, 73, 14),
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(0, 3),
+                              )
+                            ],
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/toprak2.jpg'),
+                                fit: BoxFit.cover),
+                            color: Color.fromARGB(255, 221, 73, 14),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
                       ),
-                      decoration: BoxDecoration(
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 5,
-                              blurRadius: 7,
-                              offset: Offset(0, 3),
-                            )
-                          ],
-                          image: DecorationImage(
-                              image: AssetImage('assets/images/tuketici.jpg'),
-                              fit: BoxFit.cover),
-                          color: Color.fromARGB(255, 221, 73, 14),
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
                     ),
-                  )
-                ],
+                    FlatButton(
+                      onPressed: () {},
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            left: 20, right: 5, top: 5, bottom: 30),
+                        width: 300,
+                        height: 180,
+                        child: Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(
+                                bottom: 0,
+                                top: 125,
+                              ),
+                              padding: EdgeInsets.only(
+                                  left: 10, bottom: 10, top: 15),
+                              width: 300,
+                              height: 55,
+                              child: Text(
+                                'Tüketici Olarak Katıl',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                    fontFamily: 'Quicksand'),
+                              ),
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3),
+                                  )
+                                ],
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20)),
+                                color: Color.fromARGB(255, 221, 73, 14),
+                              ),
+                            ),
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 5,
+                                blurRadius: 7,
+                                offset: Offset(0, 3),
+                              )
+                            ],
+                            image: DecorationImage(
+                                image: AssetImage('assets/images/tuketici.jpg'),
+                                fit: BoxFit.cover),
+                            color: Color.fromARGB(255, 221, 73, 14),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30))),
+                      ),
+                    )
+                  ],
+                ),
               ),
             )));
   }
